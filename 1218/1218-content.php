41f73bfdcd8961ffa3f45ab942beda35e0c713ae
CongTuan0807 <lctuan.0807@gmail.com>
2019-09-28 10:02:08 +0700

    a

--- 1218/1218-content.php
+++ 1218/1218-content.php
@@ -13,13 +13,13 @@
         <div class="swiper-container">
             <div class="swiper-wrapper">
                 <div class="swiper-slide">
-                    <img src="<?php echo $url_path ?>/images/about-image.png" alt="">
+                    <img src="<?php echo $url_path ?>/images/about-image" alt="">
                 </div>
                 <div class="swiper-slide">
-                    <img src="<?php echo $url_path ?>/images/content-rooms-1.png" alt="">
+                    <img src="<?php echo $url_path ?>/images/content-rooms-1" alt="">
                 </div>
                 <div class="swiper-slide">
-                    <img src="<?php echo $url_path ?>/images/content-rooms-3.png" alt="">
+                    <img src="<?php echo $url_path ?>/images/content-rooms-3" alt="">
                 </div>
                 
             </div>
@@ -42,7 +42,7 @@
                     <div class="tit">
                         <span class="tittle">TẦM NHÌN CỦA</span><span class="tittle tittle-sub"> CÔNG TY</span>
                     </div>
-                    <div class="border border-text"></div>
+                    <div class="border-text"></div>
                     <p class="content">Nâng cao chất lượng cuộc sống của Quý khách hàng. Sự đổi mới liên tục của thiết kế và kết cấu kỹ thuật là tầm nhìn của công ty.</p>
                 </div>
                 <!--INFO 2-->
@@ -50,7 +50,7 @@
                     <div class="tit">
                         <span class="tittle">TRÁCH NHIỆM</span><span class="tittle tittle-sub"> & NIỀM TIN</span>
                     </div>
-                    <div class="border border-text"></div>
+                    <div class="border-text"></div>
                     <p class="content">Công trình của Quý khách hàng như thể công trình của chúng tôi.</p>
                 </div>
                 <!--INFO 3-->
@@ -58,7 +58,7 @@
                     <div class="tit">
                         <span class="tittle">CHIẾN LƯỢC</span><span class="tittle tittle-sub"> KINH DOANH</span>
                     </div>
-                    <div class="border border-text"></div>
+                    <div class="border-text"></div>
                     <p class="content">Xây dựng một đội ngũ cạnh tranh và hướng tới chất lượng thiết kế cao hơn để đáp ứng các mục tiêu hoạt động của công ty.</p>
                 </div>
             </div>
